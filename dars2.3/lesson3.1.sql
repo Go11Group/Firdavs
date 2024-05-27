1e6cb3cd9845f8ffde332ed846280f8d74f46c17
Fedy1507 <gayratovfirdavs720@gmail.com>
2024-05-27 17:42:27 +0500

    all homework updating

--- dars2.3/lesson3.1.sql
+++ dars2.3/lesson3.1.sql
@@ -2,3 +2,18 @@
 UPDATE courses
 SET published_date = '2020-08-01' 
 WHERE course_id = 3;
+
+--DELETE ma'lumotlar bazasidan mavjud yozuvlarni olib tashlashga yordam beradi. 
+DELETE FROM courses WHERE course_id = 3;
+
+--GROUP BY - bir xil ma'lumotlarni guruhlarga ajratuvchi SQL bandidir. Ko'pincha natijalar to'plamini bir yoki bir nechta ustunlar bo'yicha guruhlash uchun agregat funktsiyalari (COUNT, MAX, MIN, SUM, AVG) bilan ishlatiladi.
+SELECT column1, column2, ..., aggregate_function(column_name)
+FROM table_name
+WHERE condition
+GROUP BY column1, column2, ...;
+
+--ORDER BY bu jadvallarni saralash uchun ishlatiladi. uning ASC yoki DESC qilish mumkun.
+
+SELECT column1, column2, ...
+FROM table_name
+ORDER BY column1, column2, ... ASC & DESC;

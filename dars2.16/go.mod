5773d307afe45515d1a4ebf7cf7972ef73fa3b94
Fedy1507 <gayratovfirdavs720@gmail.com>
2024-06-11 08:50:39 +0500

    dars2.16

---
+++ dars2.16/go.mod
@@ -0,0 +1,3 @@
+module n11/Firdavs/dars2.16
+
+go 1.22.2
---
+++ dars2.16/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("go context")
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	go myfunc(ctx)
+	select{
+	case <-ctx.Done():
+		fmt.Println("oh no")
+	}
+	time.Sleep(2*time.Second)
+}
+
+func myfunc(ctx context.Context)  {
+	for{
+		select{
+		case <-ctx.Done():
+			fmt.Println("vaht tugadi")
+			return
+		default:
+			fmt.Println("hello")
+		}
+		time.Sleep(500*time.Millisecond)
+	}
+}
